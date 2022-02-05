683ffedd1cbfca313c682fb9b37e9601587a80ed
Raziel Rodrigues <raziel@upoutsourcing.com>
2022-02-05 19:06:15 -0300

    output

---
+++ src/output.rb
@@ -0,0 +1,25 @@
+# output
+puts "Hello World"
+
+# declarando variaveis
+nome = "Raziel"
+idade = 2
+
+# tudo é um objeto
+nome.object_id
+nome.class
+
+# null
+nil
+
+# ate a propria classe é um objeto
+NilClass.object_id
+NilClass.superclass
+
+# pode usar p
+# tambem debuga
+
+p "agora com o p"
+
+# tambem pode usar print no caso na quebra linha
+print "sem quebra de linha"
