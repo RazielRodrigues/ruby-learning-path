f9ae3139f2d5d54490ad1ccd1dbf96d0299e6600
Raziel Rodrigues <raziel@upoutsourcing.com>
2022-02-05 19:06:41 -0300

    functions

---
+++ src/function.rb
@@ -0,0 +1,16 @@
+# funções
+    # return é opcional
+    # a ultima linha do metodo é considerado return
+
+# ficando
+def somar(x,y)
+    puts "algo"
+    x+y
+end
+
+# chamando função
+somar(2,1)
+
+# clojure
+soma = somar(2,1)
+puts soma
